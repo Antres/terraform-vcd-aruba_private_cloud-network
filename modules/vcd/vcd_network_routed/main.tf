fbbe4921f1ec58d220b34cd6cc0f178b0f06c9fa
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 08:53:13 +0200

    Update main.tf

--- modules/vcd/vcd_network_routed/main.tf
+++ modules/vcd/vcd_network_routed/main.tf
@@ -8,10 +8,20 @@ resource "vcd_network_routed" "network" {
   name            = var.name
   description     = var.description
   
-  gateway         = var.gateway
-  netmask         = var.netmask
+  gateway         = cidrhost(var.network, 1)
+  netmask         = cidrnetmask(var.network)
+}
+
+resource "vcd_nsxv_snat" "egress" {
+  count           = var.deploy ? 1 : 0
   
-  lifecycle {
-    create_before_destroy = false
-  }
+  org             = var.region.vdc.org
+  vdc             = var.region.vdc.name
+  edge_gateway    = var.region.edge.name
+  
+  network_type = "ext"
+  network_name = tolist(var.region.edge.external_network)[0].name
+
+  original_address   = var.network
+  translated_address = var.egress[0].with_addr
 }
