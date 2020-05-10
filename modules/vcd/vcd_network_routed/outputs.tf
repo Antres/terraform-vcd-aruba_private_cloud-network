88990d90a4d0192a0e4962af61e922d01918e4e6
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 08:16:03 +0200

    Update outputs.tf

--- modules/vcd/vcd_network_routed/outputs.tf
+++ modules/vcd/vcd_network_routed/outputs.tf
@@ -1,6 +1,6 @@
 output "network" {
   value = {
-    name = vcd_network_routed.network[0].name
+    name = length(vcd_network_routed.network[*]) > 0 ? vcd_network_routed.network[0].name : ""
     type = "vcd_network_routed"
     region = var.region
   }
