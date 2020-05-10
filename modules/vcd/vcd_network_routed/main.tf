a3092d541fbf10d9da6fc5b1f71a53a4e9d45e90
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 08:07:47 +0200

    Update main.tf

--- modules/vcd/vcd_network_routed/main.tf
+++ modules/vcd/vcd_network_routed/main.tf
@@ -1,8 +1,9 @@
-resource "vcd_network_isolated" "network" {
+resource "vcd_network_routed" "network" {
   count           = var.deploy ? 1 : 0
   
   org             = var.region.vdc.org
   vdc             = var.region.vdc.name
+  edge_gateway    = var.region.egw.name
 
   name            = var.name
   description     = var.description
