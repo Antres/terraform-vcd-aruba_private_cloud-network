ccef58d96f44041cfa86758b0b9a7f898aed0143
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 06:36:53 +0200

    Update main.tf

--- modules/vcd/vcd_network_isolated/main.tf
+++ modules/vcd/vcd_network_isolated/main.tf
@@ -1,11 +1,12 @@
 resource "vcd_network_isolated" "network" {
-  count = var.deploy ? 1 : 0
+  count           = var.deploy ? 1 : 0
   
-  org = var.region.vdc.org
-  vdc = var.region.vdc.name
+  org             = var.region.vdc.org
+  vdc             = var.region.vdc.name
 
-  name    = var.name
+  name            = var.name
+  description     = var.description
   
-  gateway = var.gateway
-  netmask = var.netmask
+  gateway         = var.gateway
+  netmask         = var.netmask
 }
