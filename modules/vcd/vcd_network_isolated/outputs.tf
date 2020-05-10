fff8434c82523720606b4733bfa1df89a77b784b
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 19:04:45 +0200

    Update outputs.tf

--- modules/vcd/vcd_network_isolated/outputs.tf
+++ modules/vcd/vcd_network_isolated/outputs.tf
@@ -1,5 +1,5 @@
 output "name" {
-  value           = try(vcd_network_isolated.network[0].name, "");
+  value           = try(vcd_network_isolated.network[0].name, "")
 }
 
 output "type" {
@@ -11,5 +11,5 @@ output "region" {
 }
 
 output "object" {
-  value           = try(vcd_network_isolated.network[0], "");
+  value           = try(vcd_network_isolated.network[0], "")
 }
